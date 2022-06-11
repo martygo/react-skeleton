88f5376817821c1d7a9e7e99511caa2aef59e4e7
Martins Gouveia <gouveiamartins6@gmail.com>
2022-06-11 21:54:00 +0100

    🎉 first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, sapiente! Exercitationem earum, cum a maxime quis adipisci cumque nihil veritatis odit blanditiis distinctio quasi minus consequatur, asperiores nemo, obcaecati quisquam.
+    </div>
+  )
+}
+
+export default App
